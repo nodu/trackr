a82177bc722c371b394fb4c233af6d2e50db0e15
Matt Nodurfth <mnodurft@gmail.com>
2013-12-09 16:22:59 +0800

    Added mongoid

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem "octokit", "~> 2.0"
 gem 'shotgun'
 gem 'dotenv-rails', :groups => [:development, :test]
 gem 'racksh'
-gem 'mongo'
 gem 'bundler'
 gem 'rake'
+gem 'mongoid'
+gem 'bson_ext'
+
--- app.rb
+++ app.rb
@@ -3,34 +3,45 @@ require "json"
 require 'octokit'
 require 'bundler/setup'
 require 'dotenv'
+require 'mongoid'
 require 'json/ext'
 
 Dotenv.load
+Mongoid.load!("config.yml")
+
+#model for mogoid
+class GithubData
+  include Mongoid::Document
+  field :name, type: String
+  field :contributions, type: Integer
+end
+
 
 class App < Sinatra::Base
 
 
-  before do 
-  #   @repo = Octokit.repo "chasm/symbiote"
-  #   @commit = Octokit.commits("chasm/toddycat", nil, :since => "2012-06-28T00:00:00+00:00").length
-    @x=Octokit.client.contributors("chasm/symbiote")
+  # before do 
+  # #   @repo = Octokit.repo "chasm/symbiote"
+  # #   @commit = Octokit.commits("chasm/toddycat", nil, :since => "2012-06-28T00:00:00+00:00").length
+  #   @x=Octokit.client.contributors("chasm/symbiote")
     
-    @student=[]
-    @data=[]
+  #   @student=[]
+  #   @data=[]
 
-    @x.each do |i|
-      @student << i.login
-      @data << i.contributions
-    end 
+  #   @x.each do |i|
+  #     @student << i.login
+  #     @data << i.contributions
+  #   end 
 
 
 
-  configure do
-    conn = MongoClient.new("localhost", 27017)
-    set :mongo_connection, conn
-    set :mongo_db, conn.db('test')
 
-  end
+  # configure do
+  #   conn = MongoClient.new("localhost", 27017)
+  #   set :mongo_connection, conn
+  #   set :mongo_db, conn.db('test')
+
+  # end
 
   get '/course' do 
     erb :course
@@ -41,6 +52,10 @@ class App < Sinatra::Base
   end
 
   get '/github' do 
+    @git = GithubData.new({name: "Bruno", contributions: 5})
+    @git.save
+    puts @git
+
     erb :github
   end
 
@@ -61,6 +76,7 @@ class App < Sinatra::Base
   end
 
   get "/" do
+    @x = []
     erb :index
   end
 
