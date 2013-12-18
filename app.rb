116d4bac050db8a50a3704d55538204a4097517c
vbisqebu <hatch.josiah@gmail.com>
2013-12-18 16:42:15 +0800

    middle of implementing collision detection d3

--- app.rb
+++ app.rb
@@ -16,6 +16,50 @@ end
 
 class App < Sinatra::Base
 
+  ['/github', '/student'].each do |path|
+    before path do
+      @data = GithubData.all
+      # @data.each do |x|
+      #   puts x.wdi1.contributions
+      # end
+      # @contrib = []
+      # @name = []
+      @contrib = []
+      @students_data=[]
+      
+      @data.each do |item|
+        # puts "hey joker"
+        # puts item.wdi1["group_repos"]["repo1"]["data"][0]['contributions']
+        
+        item.wdi1["group_repos"]["repo1"]["data"].each do |i|
+          # @contrib << i['contributions']
+          # @name << i['login']
+          @contrib << {login: i["login"], contributions: i["contributions"]}
+
+        end
+        
+        puts item.wdi1["students"][1]["login"]
+        item.wdi1["students"].each do |i|
+          #puts i["login"], i["gravatar_id"], i["user_repos"]
+          @students_data << { 
+            login: i["login"], 
+            gravatar_id: i["gravatar_id"], 
+            user_repos: i["user_repos"], 
+            user_commits: i["user_commits"],
+            user_watching: i["user_watching"],
+            user_starred: i["user_starred"],
+            user_total_streak: i["user_total_streak"],
+            user_max_streak: i["user_max_streak"],
+            user_streak_days: i["user_streak_days"]
+          }
+        end
+
+        
+      end
+    end
+  end
+
+
   get '/course' do 
     erb :course
   end
@@ -25,36 +69,6 @@ class App < Sinatra::Base
   end
 
   get '/github' do 
-    @data = GithubData.all
-    # @data.each do |x|
-    #   puts x.wdi1.contributions
-    # end
-    # @contrib = []
-    # @name = []
-    @contrib = []
-    @students_data=[]
-    
-    @data.each do |item|
-      # puts "hey joker"
-      # puts item.wdi1["group_repos"]["repo1"]["data"][0]['contributions']
-      
-      item.wdi1["group_repos"]["repo1"]["data"].each do |i|
-        # @contrib << i['contributions']
-        # @name << i['login']
-        @contrib << {login: i["login"], contributions: i["contributions"]}
-
-      end
-      
-      puts item.wdi1["students"][1]["login"]
-      item.wdi1["students"].each do |i|
-        #puts i["login"], i["gravatar_id"], i["user_repos"]
-        @students_data << { login: i["login"], gravatar_id: i["gravatar_id"] }
-      end
-
-      
-    end
-
-
     erb :github
   end
 
